714032579bf4040f671ba75da570b4fc7277a743
Justin Seyster <justin@seyster.org>
2013-11-20 03:42:38 -0500

    Added rate limit.  Sorry.

--- xmas.web/server.js
+++ xmas.web/server.js
@@ -4,13 +4,51 @@ var express = require('express');
 var http = require('http');
 var socketio = require('socket.io');
 
-var lightArray = [ false, false, false, false, false, false, false, false ];
+/* After a light turns on, it cannot turn on again until after this
+ * delay passes. The delay servers as a rate limit, preventing the
+ * light from turning on and off many times a second. */
+var LIGHT_ON_DELAY = 500;
+
+function Light() {
+    this.enabled = false;
+    this.lastLit = 0;
+}
+
+function buildLightArray(numLights) {
+    var outArray = new Array();
+    for (var i = 0; i < numLights; i++)
+	outArray.push(new Light());
+
+    return outArray;
+}
+
+var lightArray = buildLightArray(8);
+
+function disableLight(light) {
+    light.enabled = false;
+}
+
+function enableLight(light) {
+    var now = new Date();
+    if (now.getTime() >= light.lastLit + 2000) {
+	light.enabled = true;
+	light.lastLit = now.getTime();
+    }
+}
+
+function toggleLight(light) {
+    console.log(light);
+    if (light.enabled)
+	disableLight(light);
+    else
+	enableLight(light);
+}
 
 function lightStateBitField() {
     var bitfield = 0;
     for (var i = lightArray.length - 1; i >= 0; i--) {
-        bitfield <<= 1;
-        bitfield |= lightArray[i] ? 1 : 0;
+	bitfield <<= 1;
+	bitfield |= lightArray[i].enabled ? 1 : 0;
     }
 
     return  bitfield;
@@ -28,7 +66,7 @@ io.sockets.on('connection', function(socket) {
 
     socket.on('light toggle', function(data) {
 	if (data >= 0 && data < lightArray.length) {
-	    lightArray[data] = !lightArray[data];
+	    toggleLight(lightArray[data]);
 
             var bitfield = lightStateBitField();
             socket.emit('server update', bitfield);
