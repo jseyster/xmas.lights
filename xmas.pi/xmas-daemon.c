e8bb15a91bdc81e913b739b0104850ff424230df
Justin Seyster <justin@seyster.org>
2013-11-20 05:29:47 -0500

    Daemonized xmas-daemon.

--- xmas.pi/xmas-daemon.c
+++ xmas.pi/xmas-daemon.c
@@ -3,10 +3,13 @@
  * listens for commands on a named pipe. */
 
 #include <assert.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <syslog.h>
 #include <unistd.h>
 
 #include <sys/types.h>
@@ -19,7 +22,7 @@ void create_pipe()
   int result;
 
   result = mknod(PIPE_NAME, S_IFIFO | 00622, 0);
-  if (result != 0) {
+  if (result != 0 && errno != EEXIST) {
     perror(PIPE_NAME);
     exit(1);
   }
@@ -41,20 +44,30 @@ void command_loop(FILE *pipe)
   uint8_t bitfield;
 
   while (fread(&bitfield, 1, 1, pipe) == 1) {
-    printf("Command: %d\n", bitfield);
+    syslog(LOG_NOTICE, "Command: %d\n", bitfield);
   }
 
   if (ferror(pipe))
-    fprintf(stderr, "Error reading from command pipe\n");
+    syslog(LOG_ERR, "Error reading from command pipe: %s\n", strerror(errno));
 
   fclose(pipe);
 }
 
 int main()
 {
+  int result;
   FILE *pipe;
 
   create_pipe();
+
+  openlog("xmas-daemon", 0, LOG_DAEMON);
+  syslog(LOG_NOTICE, "Listening for Christmas joy\n");
+
+  result = daemon(0, 0);
+  if (result != 0) {
+    perror("daemon");
+    exit(1);
+  }
 
   while (1) {
     pipe = open_pipe();
@@ -62,5 +75,4 @@ int main()
 
     command_loop(pipe);
   }
-
 }
