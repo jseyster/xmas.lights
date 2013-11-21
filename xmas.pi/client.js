774a39aeb7ac932c82dd5e790be8fb961054c99e
Justin Seyster <justin@seyster.org>
2013-11-21 04:52:31 -0500

    Make all the node.js stuff smarter about which server it connects to.

--- xmas.pi/client.js
+++ xmas.pi/client.js
@@ -8,9 +8,18 @@ var PIPE_NAME = '/tmp/xmas';
 var fs = require('fs');
 var io = require('socket.io-client');
 
+var serverName;
+if (process.argv.length === 3) {
+    serverName = process.argv[2];
+}
+else {
+    console.log("client.js takes the server to connect to as its only argument");
+    process.exit(1);
+}
+
 var pipe = fs.openSync(PIPE_NAME, 'w');
 
-var socket = io.connect('http://localhost:8080');
+var socket = io.connect(serverName);
 socket.on('server update', function(data) {
     var buffer = new Buffer([data]);
     fs.writeSync(pipe, buffer, 0, 1);
