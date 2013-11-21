b10475ae34062f460d9a6881da87d6df8c93ad7e
Justin Seyster <justin@seyster.org>
2013-11-21 02:37:54 -0500

    Added idle sequence.

--- xmas.web/server.js
+++ xmas.web/server.js
@@ -4,10 +4,15 @@ var express = require('express');
 var http = require('http');
 var socketio = require('socket.io');
 
+var sequence = require('./sequence.json');
+
 /* After a light turns on, it cannot turn on again until after this
  * delay passes. The delay servers as a rate limit, preventing the
  * light from turning on and off many times a second. */
 var LIGHT_ON_DELAY = 500;
+
+var lastCommandTime = new Date();
+var inIdle = false;
 
 function Light() {
     this.enabled = false;
@@ -34,6 +39,11 @@ function enableLight(light) {
 	light.enabled = true;
 	light.lastLit = now.getTime();
     }
+}
+
+function disableAll() {
+    for (var i = 0; i < lightArray.length; i++)
+	lightArray[i].enabled = false;
 }
 
 function lightStateBitField() {
@@ -66,6 +76,7 @@ io.sockets.on('connection', function(socket) {
 	if (data >= 0 && data < lightArray.length) {
 	    enableLight(lightArray[data]);
 	    updateClients(socket);
+	    lastCommandTime = new Date();
 	}
     });
 
@@ -73,6 +84,56 @@ io.sockets.on('connection', function(socket) {
 	if (data >= 0 && data < lightArray.length) {
 	    disableLight(lightArray[data]);
 	    updateClients(socket);
+	    lastCommandTime = new Date();
 	}
     });
+
+    /* If we go too long without activity, begin a default
+     * sequence. */
+    function idleCheck() {
+	var timeNow = new Date();
+	if (!inIdle && lastCommandTime.getTime() + 5000 < timeNow.getTime())
+	    doSequence(sequence.events, socket);
+    }
+    setInterval(idleCheck, 1000);
+
 });
+
+function doSequence(events, socket) {
+    var timeBegan = new Date();
+    var timeIdleBegan = lastCommandTime;  // The time that the idle period began.
+
+    inIdle = true;
+    disableAll();
+    updateClients(socket);
+
+    var executeEvents = function(nextEvent, nextEventTime) {
+	// If there has been a command since we last went idle, then we're done here.
+	if (lastCommandTime.getTime() > timeIdleBegan.getTime()) {
+	    inIdle = false;
+	    return;
+	}
+
+	var timeNow = new Date();
+	var elapsed = timeNow.getTime() - timeBegan.getTime();
+
+	while (elapsed >= nextEventTime) {
+	    var index = events[nextEvent].index;
+	    if (index >= 0 && index < lightArray.length)
+		lightArray[index].enabled = events[nextEvent].enabled;
+
+	    nextEvent++;
+	    if (nextEvent >= events.length)
+		nextEvent = 0;
+
+	    nextEventTime += events[nextEvent].afterDelay;
+	}
+	updateClients(socket);
+
+	var delay = nextEventTime - elapsed;
+	setTimeout(executeEvents, delay, nextEvent, nextEventTime);
+    };
+
+    if (events.length > 0)
+	executeEvents(0, events[0].afterDelay);
+}
