73b0d87d8a753438360f035e15aa051ac6b8b8bc
Justin Seyster <justin@seyster.org>
2013-11-21 04:22:31 -0500

    Actually wire the daemon to the real GPIO pins.

--- xmas.pi/xmas-daemon.c
+++ xmas.pi/xmas-daemon.c
@@ -11,16 +11,32 @@
 #include <string.h>
 #include <syslog.h>
 #include <unistd.h>
+#include <wiringPi.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 
 #define PIPE_NAME "/tmp/xmas"
+#define NUM_GPIO_PINS 8
 
-void create_pipe()
+static void init_wiring()
+{
+  int i;
+
+  if (wiringPiSetup() == -1) {
+    fprintf(stderr, "Failed to set up wiringPi. Do you have root priveleges?\n");
+    exit(1);
+  }
+
+  for (i = 0; i < NUM_GPIO_PINS; i++)
+    pinMode(i, OUTPUT);
+}
+
+static void create_pipe()
 {
   int result;
 
+  umask(0);  /* So we can set global write permissions for /tmp/xmas */
   result = mknod(PIPE_NAME, S_IFIFO | 00622, 0);
   if (result != 0 && errno != EEXIST) {
     perror(PIPE_NAME);
@@ -28,7 +44,7 @@ void create_pipe()
   }
 }
 
-FILE *open_pipe()
+static FILE *open_pipe()
 {
   FILE *pipe = fopen(PIPE_NAME, "r");
   if (pipe == NULL) {
@@ -39,13 +55,22 @@ FILE *open_pipe()
   return pipe;
 }
 
-void command_loop(FILE *pipe)
+static void write_pins(uint8_t bitfield)
+{
+  int i;
+
+  for (i = 0; i < NUM_GPIO_PINS; i++) {
+    digitalWrite(i, bitfield & 1);
+    bitfield >>= 1;
+  }
+}
+
+static void command_loop(FILE *pipe)
 {
   uint8_t bitfield;
 
-  while (fread(&bitfield, 1, 1, pipe) == 1) {
-    syslog(LOG_NOTICE, "Command: %d\n", bitfield);
-  }
+  while (fread(&bitfield, 1, 1, pipe) == 1)
+    write_pins(bitfield);
 
   if (ferror(pipe))
     syslog(LOG_ERR, "Error reading from command pipe: %s\n", strerror(errno));
@@ -58,6 +83,7 @@ int main()
   int result;
   FILE *pipe;
 
+  init_wiring();
   create_pipe();
 
   openlog("xmas-daemon", 0, LOG_DAEMON);
